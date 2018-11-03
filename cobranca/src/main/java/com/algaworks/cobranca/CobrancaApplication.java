fa3364e0d8f9e43975d5e1a2501390695d60d783
Douglas Ferreira <douglas0003@hotmail.com>
2018-11-03 16:45:14 -0300

    Alterações para implementar no tomcat sem a tools usando o tf, implementando também a busca completa.

--- cobranca/src/main/java/com/algaworks/cobranca/CobrancaApplication.java
+++ cobranca/src/main/java/com/algaworks/cobranca/CobrancaApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
 @SpringBootApplication
@@ -20,4 +22,11 @@ public class CobrancaApplication {
 		return new FixedLocaleResolver(new Locale("pt", "BR"));
 	}
 	
+	public static class MvcConfig extends WebMvcConfigurerAdapter {
+		
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addRedirectViewController("/", "/titulos");
+		}
+	}
 }
